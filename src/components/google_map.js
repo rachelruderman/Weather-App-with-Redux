f8dca5b0b47eae5d8bd0242f1fc4e38dd4ade0e2
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-01 12:47:51 -0700

    integrated google maps api and style

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 
 class GoogleMap extends Component {
+
   componentDidMount(){
+    //this is how we create an embedded google map
+    //it takes an html element as the first argument, and then renders an embedded map into it
+    //this is generally how we make 3rd party libraries work nicely with React
     new google.maps.Map(this.refs.map, {
+      //zoom of 12 is about the screen width of a city
       zoom: 12,
+      //this tells google maps where we want to center the map on
       center: {
         lat: this.props.lat,
-        lng: this.props.lng 
+        lng: this.props.lon
       }
     })
   }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
@@ -9,10 +10,12 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp)
     const pressures = cityData.list.map(weather => weather.main.pressure)
     const humidities = cityData.list.map(weather => weather.main.humidity)
+    //this line right here says: find the coord object, grab the lon and lat properties off of it, and assign them to two new variables called lon and lat
+    const {lon, lat} = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color='orange' units="K" /></td>
         <td><Chart data={pressures} color='green' units="hPa" /></td>
         <td><Chart data={humidities} color='black' units="%" /></td>
