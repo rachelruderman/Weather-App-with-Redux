c2c296b108844dd929b9479ac20fc2f2eec34fe6
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-01 11:02:07 -0700

    renderWeather function working in weather_list.js container

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,39 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+
+  renderWeather(cityData){
+    return (
+      <tr>
+        <td>{cityData.city.name}</td>
+      </tr>
+    )
+  }
+
   render(){
     return (
       <table className='table table-hover'>
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
-          
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 }
+
+function mapStateToProps({weather}){
+  //{weather} same as state.weather
+  return {weather}
+  //whenever we have a key/value that are identical, we can shrink it to one word
+}
+
+export default connect(mapStateToProps)(WeatherList)
