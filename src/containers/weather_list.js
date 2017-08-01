19460023628b5c7b3f0871c9dbff6f2786f7f359
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-01 12:26:51 -0700

    added units to chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -13,9 +13,9 @@ class WeatherList extends Component {
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color='orange' /></td>
-        <td><Chart data={pressures} color='green' /></td>
-        <td><Chart data={humidities} color='black' /></td>
+        <td><Chart data={temps} color='orange' units="K" /></td>
+        <td><Chart data={pressures} color='green' units="hPa" /></td>
+        <td><Chart data={humidities} color='black' units="%" /></td>
       </tr>
     )
   }
@@ -26,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
